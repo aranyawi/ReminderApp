3a87b4880676f8cb419133c91fe987849c48290f
aranyawi <105372777+aranyawi@users.noreply.github.com>
2022-12-29 08:04:58 +0700

    Create icons.js

---
+++ constants/icons.js
@@ -0,0 +1,12 @@
+const home = require("../icons/home.png");
+const noti = require("../icons/noti.png");
+const profile = require("../icons/profile.png");
+const review = require("../icons/review.png");
+
+export default {
+    home,
+    noti,
+    review,
+    profile,
+    
+}
